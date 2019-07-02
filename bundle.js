d27cb0ffe89afc98b112b338851889300af83662
Sam Hodak <slhodak@gmail.com>
2019-07-02 13:45:05 -0700

    Implement simple bundler

---
+++ bundle.js
@@ -0,0 +1,16 @@
+const scripts = [
+  'demoFileA.js',
+  'demoFileB.js'
+];
+
+let reference = document.getElementsByClassName('scripts')[0];
+
+function addScripts(scripts, reference) {
+  scripts.forEach(title => {
+    let script = document.createElement('script');
+    script.src = title;
+    document.body.insertBefore(script, reference);
+  });
+}
+
+addScripts(scripts, reference);
